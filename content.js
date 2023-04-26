4ddead84b743e931b39456c4eff1db3c1dd0f2b1
masachika <masachika.kmd@gmail.com>
2023-04-27 01:54:41 +0900

    [update] user nameとスクロールバーの修正

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 function customizePage() {
-  // Change the text using the specified XPath
-  const changeTextXPath = '//*[@id="headlessui-menu-button-:Rq5hd6:"]/div[2]';
-  const changeTextNode = document.evaluate(changeTextXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  // Change the text using a CSS selector
+  const changeTextSelector = 'button[id^="headlessui-menu-button"] > div:nth-child(2)';
+  const changeTextNode = document.querySelector(changeTextSelector);
   if (changeTextNode) {
     changeTextNode.textContent = 'USER';
   }
@@ -16,7 +16,7 @@ function customizePage() {
   // Change the width and center-align the target element
   const targetElement = document.querySelector('.absolute.bottom-0.left-0.w-full');
   if (targetElement) {
-    targetElement.style.width = '98.5%';  // 1.5 rem のスクロールバー分を引いている
+    targetElement.style.width = 'calc(100% - 1rem)';  // 1rem のスクロールバー分を引いている
   }
 }
 
@@ -26,7 +26,7 @@ function observeDOMChanges() {
 
   const observer = new MutationObserver((mutationsList, observer) => {
     for (const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
         customizePage();
       }
     }
