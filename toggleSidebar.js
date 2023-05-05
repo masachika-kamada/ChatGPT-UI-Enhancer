2d41ddaa4672f9d6db97a0565933f08147cf9b08
masachika <masachika.kmd@gmail.com>
2023-05-05 19:10:56 +0900

    [fix] #19 ウィンドウ幅によるフラグ値の反転を解決

--- toggleSidebar.js
+++ toggleSidebar.js
@@ -40,11 +40,33 @@ export function createSidebarToggleButton() {
 
 export function checkSidebarAndButtonVisibility() {
   const button1 = document.querySelector('.toggle-tab-button');
-  const button2 = document.querySelector('.toggle-chat-history-button');  // Assume this is the class for the other button
-  const breakPoint = 768;  // This is the break point
+  const button2 = document.querySelector('.toggle-chat-history-button');
+  const breakPoint = 768;
 
   if (button1 && button2) {
     const displayValue = (window.innerWidth <= breakPoint) ? 'none' : '';
+
+    // When the window width is larger than the breakpoint, ensure the sidebar is visible
+    if (window.innerWidth > breakPoint) {
+      const tabXPath = '//*[@id="__next"]/div[2]/div[1]';
+      const tabNode = document.evaluate(tabXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (tabNode) {
+        tabNode.style.display = '';
+        button1.style.left = '260px';
+        const arrow = button1.querySelector('.toggle-tab-arrow');
+        if (arrow) {
+          arrow.style.transform = 'translate(-50%, -50%) scaleX(-1)';
+        }
+        const isVisible = true;
+
+        // Fire a custom event
+        const event = new CustomEvent('sidebarToggled', {
+          detail: { isVisible }
+        });
+        document.dispatchEvent(event);
+      }
+    }
+
     button1.style.display = displayValue;
     button2.style.display = displayValue;
   }
